e30f0bcb80df472255302d8c765cdfb1cdd71884
Hichem AIT OUAKLI <alterhichem99@gmail.com>
2023-02-06 22:35:19 +0100

    Some practice

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, ref } from 'vue'
+import Navbar from "./components/Navbar.vue";
+import Counter from "./components/Counter.vue";
+
+
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
-  <main>
-    <TheWelcome />
+  <Navbar></Navbar>
+
+  <main class="wrapper">
+    <div class="main-title">
+      <h1>Choose project:</h1>
+    </div>
+    <Counter></Counter>
   </main>
-  
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.main-title {
+  text-align: center;
+  padding-bottom: 1rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper {
+  background-color: #333;
+  margin: 1rem;
+  padding: 1rem 10rem 100rem 5rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.image {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+b {
+  font-weight: bold;
 }
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+    import { reactive, ref } from 'vue'
+    const state = reactive({
+        count: 0,
+    })
+
+    const increment = () => {
+        state.count++;
+    }
+
+    const decrement = () => {
+        if (state.count >= 1) {
+            state.count--;
+        }
+    }
+
+</script>
+
+<template>
+
+    <div>
+        <h1> Counter </h1>
+        <h4>The current count is :</h4>
+        <h1>{{ state.count }}</h1>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+    </div>
+
+</template>
+
+<style scoped>
+button{
+  width: 5em;
+  height: 5em;
+  font-size: 1rem;
+  font-weight: bold;
+}
+
+</style>
